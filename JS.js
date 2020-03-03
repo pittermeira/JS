6e189d86019ffa75af14be27ef28c63ad1669600
Pitter Meira <pittermeira@hotmail.com>
2020-03-02 21:01:37 -0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,12 @@
+var fila = [1,2,3,4,5];
+fila.push(6);
+fila.push(7);
+fila.push(8);
+fila.push(9);
+console.log(fila)
+fila.pop()
+console.log(fila)
+fila.unshift(0)
+console.log(fila)
+fila.splice(5,2,"bar")
+console.log(fila)
